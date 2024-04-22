2b631989ec236d850707643dda7da53caaf7677e
PatrikMasurica <113673922+PatrikMasurica@users.noreply.github.com>
2024-04-22 09:56:58 -0700

    done

--- Assignments/JS/Quiz Application with Timer starter_2/js/script.js
+++ Assignments/JS/Quiz Application with Timer starter_2/js/script.js
@@ -10,8 +10,14 @@ const timer_sec = document.querySelector('.timer_sec');
 const time_line = document.querySelector('.time_line');
 const next_btn = document.querySelector('.next_btn');
 const total_que = document.querySelector('.total_que');
+const result_box = document.querySelector('.result_box');
+const score_text = document.querySelector('.score_text');
 
+let timerInterval;
 
+let points = 0;
+let totalNumbofQuestions = questions.length;
+let qCounter = 0;
 
 
 let timer = 15;
@@ -23,29 +29,28 @@ start_btn.addEventListener('click', function () {
     start_btn.style.display = 'none';
 })
 
-
 quit_btn.addEventListener('click', function () {
     info_box.classList.remove('activeInfo');
     start_btn.style.display = 'block';
 
 })
 
-
 continue_btn.addEventListener('click', function () {
     info_box.classList.remove('activeInfo');
     quiz_box.classList.add('activeQuiz');
-    loadQuestion(0);
+    loadQuestion(qCounter);
 })
 
 function loadQuestion(index) {
     let allOptions = questions[index].options;
     que_text.innerHTML = questions[index].question;
+    option_list.innerHTML = '';
 
     for (let i = 0; i < allOptions.length; i++) {
         option_list.innerHTML += `<div class="option" onclick="optionSelect(this,${index},${i})"><span>${allOptions[i]}</span ></div > `;
     }
 
-    let timerInterval = setInterval(timerCount, 1000);
+    timerInterval = setInterval(timerCount, 1000);
 
 }
 
@@ -64,11 +69,12 @@ let timerCount = function () {
 }
 
 let optionSelect = function (opt, q, o) {
-
     for (let i = 0; i < 4; i++) {
 
         if (questions[q].options[i] == questions[q].answer) {
             option_list.children[i].classList.add("correct");
+            points++;
+
         }
         option_list.children[i].classList.add("disabled");
 
@@ -77,10 +83,33 @@ let optionSelect = function (opt, q, o) {
         opt.classList.add("incorrect");
 
     }
+    clearInterval(timerInterval);
 
-
+    qCounter++;
     next_btn.classList.add('show');
-    total_que.innerHTML = '1/6 question';
+    total_que.innerHTML = (qCounter) + '/6 question';
+    if (qCounter === totalNumbofQuestions) {
+        quiz_box.classList.remove('activeQuiz');
+        result_box.classList.add('activeResult');
+        score_text.textContent = points + " from " + totalNumbofQuestions;
+
+    }
+
+}
+
+next_btn.addEventListener('click', function () {
+    loadQuestion(qCounter);
+    resetAllQuestions();
+})
+
+let resetAllQuestions = function () {
+
+    timer_sec.textContent = 15;
+    lineTimer = 0;
+    next_btn.classList.add('show');
+    time_line.style.width = 0;
+    timerCount = 0;
+    timerInterval = setInterval(timerCount, 1000);
 
 }
 
