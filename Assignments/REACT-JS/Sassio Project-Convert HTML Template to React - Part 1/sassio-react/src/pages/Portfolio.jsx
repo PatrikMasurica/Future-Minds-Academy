b8b4d06767bf1f3f5b9a11f6062b0edffa992c3d
PatrikMasurica <113673922+PatrikMasurica@users.noreply.github.com>
2024-05-27 00:37:18 +0200

    Restructured files of Sassio React Project

--- Assignments/REACT-JS/Sassio Project-Convert HTML Template to React - Part 1/sassio-react/src/pages/Portfolio.jsx
+++ Assignments/REACT-JS/Sassio Project-Convert HTML Template to React - Part 1/sassio-react/src/pages/Portfolio.jsx
@@ -5,7 +5,9 @@ import PortfolioHtml from "../components/PortfolioHtml";
 export default function Portfolio() {
     return (
         <div className="App">
+
             <PortfolioHtml />
+
         </div>
 
     )
