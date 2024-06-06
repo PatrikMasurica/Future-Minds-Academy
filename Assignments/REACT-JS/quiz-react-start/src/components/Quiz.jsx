d7547a699e70400e5c41bf9082fb164f1cb116ba
PatrikMasurica <113673922+PatrikMasurica@users.noreply.github.com>
2024-06-07 00:55:20 +0200

    done

--- Assignments/REACT-JS/quiz-react-start/src/components/Quiz.jsx
+++ Assignments/REACT-JS/quiz-react-start/src/components/Quiz.jsx
@@ -1,11 +1,15 @@
 import Questions from '../assets/js/questions';
 import { useState } from 'react';
+import quizCompleteScreen from '../assets/quiz-complete.png'
+import QuizTimer from './QuizTimer';
 
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+
+    const isQuizComplete = activeQuestionIndex === Questions.length;
 
     function handleAnswer(selectedAnswer) {
         setUserAnswers(
@@ -15,19 +19,33 @@ export default function Quiz() {
         )
     }
 
+    if (isQuizComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteScreen} alt="Quiz Completed" />
+                <h2>Quiz Complete</h2>
+            </div>
+        )
+    }
+
+    const shuffleAnswers = [...Questions[activeQuestionIndex].answers.sort((a, b) => Math.random() - 0.5)];
+
+
     return (
         <>
             <main>
                 <div id="quiz">
                     <div id="question">
+                        <QuizTimer />
                         <h2>{Questions[activeQuestionIndex].text}</h2>
-                        <ul id="answers">{
-                            Questions[activeQuestionIndex].answers.map((answer) =>
-                                <li key={answer} className='answer'>
-                                    <button onClick={() => handleAnswer(answer)}>{answer}</button>
-                                </li>
-                            )
-                        }
+                        <ul id="answers">
+                            {
+                                shuffleAnswers.map((answer) =>
+                                    <li key={answer} className='answer'>
+                                        <button onClick={() => handleAnswer(answer)}>{answer}</button>
+                                    </li>
+                                )
+                            }
                         </ul>
                     </div>
                 </div>
