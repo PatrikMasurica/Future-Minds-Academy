be799d7c618032f6f159443d6e105176c7501787
PatrikMasurica <113673922+PatrikMasurica@users.noreply.github.com>
2024-06-20 19:14:10 +0200

    pushhhh it baby

--- Assignments/REACT-JS/to-do-app/todo-app/src/components/ToDoForm.js
+++ Assignments/REACT-JS/to-do-app/todo-app/src/components/ToDoForm.js
@@ -3,12 +3,17 @@ import { useState } from "react";
 export default function ToDoForm({ addTask }) {
 
     const [task, setTask] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (task.trim()) {
-            addTask(task);
+        if (task.trim() && date && time) {
+            addTask(task, date, time);
             setTask('');
+            setDate('')
+            setTime('');
         }
     }
 
@@ -20,6 +25,16 @@ export default function ToDoForm({ addTask }) {
                     placeholder="Add a new task"
                     value={task}
                     onChange={(e) => setTask(e.target.value)} />
+                <input
+                    type="date"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                />
+                <input
+                    type="time"
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
+                />
                 <button type="submit">Add</button>
             </form>
         </>
