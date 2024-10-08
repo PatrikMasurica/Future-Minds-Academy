4a85596ea271b8ebbe45590dbd98e5b0a0e4df34
patrik-startkiwi <113673922+PatrikMasurica@users.noreply.github.com>
2024-10-08 20:12:10 +0200

    filesystem learn

---
+++ Assignments/NodeJS/NodeJS/Session-3/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+//ME LEXU FAJLLIN
+// async function main() {
+//     try {
+//         const data = await fs.readFile('text.txt');
+//         console.log(data.toString())
+//     }
+//     catch (err) {
+//         throw err;
+//     }
+
+// }
+
+//ME SHKRU NFAJLL
+// async function main() {
+//     try {
+//         fs.writeFile('text1.txt', 'Kete file e kemi krijuar me NodeJs')
+//     }
+//     catch (err) {
+//         throw err;
+//     }
+
+// }
+
+//ME UPDATE FAJLLIN e bon fs.appendFile
+
+//ME DELETE E BON fs.unlink
+
+
+main();
+
+
